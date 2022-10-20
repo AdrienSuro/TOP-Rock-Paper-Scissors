a99650ccdf2b2762fc80eb01971a91a90721b569
AdrienSuro <adrien.surowiec@protonmail.com>
2022-10-20 22:10:35 +0200

    Add first step - computer choice

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-console.log("Hello World!");
-
-function display() {  
-    alert("Hello World!");  
-    }  
+function getComputerChoice() {
+    let computerChoice = '';
+    randomNumber = Math.floor(Math.random() * 3);
+    if randomNumber = 0 {
+        computerChoice = "Rock";
+    }
+    else if randomNumber = 1 {
+        computerChoice = "Paper";
+    } 
+    else if randomNumber = 3 {
+        computerChoice = "Scissors";
+    }
+}
