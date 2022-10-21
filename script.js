d9eb299945c748090bbf1ad148eab51e82f96131
AdrienSuro <adrien.surowiec@protonmail.com>
2022-10-21 21:02:30 +0200

    Add communication between .js and .html

--- script.js
+++ script.js
@@ -1,43 +1,50 @@
-function getComputerChoice() {
-    let computerChoice = '';
-    randomNumber = Math.floor(Math.random() * 3);
-    console.log(randomNumber);
-    if (randomNumber == 0) {
-        computerChoice = "Rock";
+function getInputValue() {
+    let playerChoice = document.getElementById("inputId").value;
+    // console.log(playerChoice);
+    singleRound(playerChoice, getComputerChoice());
+}
+
+function singleRound(player, computer) {
+    if (player === computer) {
+        alert("It's a draw!");
     }
-    else if (randomNumber == 1) {
-        computerChoice = "Paper";
-    } 
-    else if (randomNumber == 0) {
-        computerChoice = "Scissors";
+    else if (computer === "Paper" && player === "Rock") {
+        alert ("You lost, you're a looser.")
+    }
+    else if (computer === "Paper" && player === "Scissors") {
+        alert ("You won, lucky bastard.")
+    }
+    else if (computer === "Rock" && player === "Paper") {
+        alert ("You won, lucky bastard.")        
+    }
+    else if (computer === "Rock" && player === "Scissors") {
+        alert ("You lost, you're a looser.")
+    }
+    else if (computer === "Scissors" && player === "Rock") {
+        alert ("You won, lucky bastard.")        
+    }
+    else if (computer === "Scissors" && player === "Paper") {
+        alert ("You lost, you're a looser.")
     }
 }
-console.log("You're great");
-console.log(getComputerChoice());
 
-let playerChoice = document.getElementById("playerChoice").value;
-console.log(playerChoice);
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice = '';
+    if (randomNumber == 0) {
+        computerChoice = "Rock";
+        document.getElementById("computerChoice").innerHTML = computerChoice;
+        return "Rock";
+    }
+    else if (randomNumber == 1) {
+        computerChoice = 'Paper';
+        document.getElementById("computerChoice").innerHTML = computerChoice;
+        return "Paper";
+    } 
+    else if (randomNumber == 0) {
+        computerChoice = 'Scissors';
+        document.getElementById("computerChoice").innerHTML = computerChoice;
+        return "Scissors";
+    }
+}
 
-// function singleRound() {
-//     if playerChoice === computerChoice {
-//         alert("It's a draw!");
-//     }
-//     else if computerChoice === "Paper" && playerChoice === "Rock" {
-//         alert ("You lost, you're a looser.")
-//     }
-//     else if computerChoice === "Paper" && playerChoice === "Scissors" {
-//         alert ("You won, lucky bastard.")
-//     }
-//     else if computerChoice === "Rock" && playerChoice === "Paper" {
-//         alert ("You won, lucky bastard.")        
-//     }
-//     else if computerChoice === "Rock" && playerChoice === "Scissors" {
-//         alert ("You lost, you're a looser.")
-//     }
-//     else if computerChoice === "Scissors" && playerChoice === "Rock" {
-//         alert ("You won, lucky bastard.")        
-//     }
-//     else if computerChoice === "Scissors" && playerChoice === "Paper" {
-//         alert ("You lost, you're a looser.")
-//     }
-// }
