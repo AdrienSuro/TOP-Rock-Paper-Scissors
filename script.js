1b55d9d005ee95815bfe04f11ee3c907d5417b22
AdrienSuro <adrien.surowiec@protonmail.com>
2022-10-23 08:35:50 +0200

    Add a solution working inside the Console only

--- script.js
+++ script.js
@@ -1,50 +1,81 @@
-function getInputValue() {
-    let playerChoice = document.getElementById("inputId").value;
-    // console.log(playerChoice);
-    singleRound(playerChoice, getComputerChoice());
+let userPoints = 0;
+let computerPoints = 0;
+let userChoice = '';
+let computerChoice = '';
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        singleRound();
+    }
+    if (userPoints > computerPoints) {
+        console.log("You're the best, you won the game!");
+    }
+    else if (userPoints < computerPoints) {
+        console.log("You lost... Try again!");
+    }
+    else {
+        console.log("Oh oh... Seems like it's a final draw. Equality for all! The communist inside you shall be happy.");
+    }
 }
 
-function singleRound(player, computer) {
-    if (player === computer) {
-        alert("It's a draw!");
+function singleRound() {
+    getComputerChoice();
+    getUserChoice();
+    if (userChoice === computerChoice) {
+        console.log("It's a draw!");
+        userPoints += 1;
+        computerPoints += 1;
     }
-    else if (computer === "Paper" && player === "Rock") {
-        alert ("You lost, you're a looser.")
+    else if (computerChoice === "Paper" && userChoice === "Rock") {
+        console.log(`You lost, ${computerChoice} beats ${userChoice}`);
+        computerPoints +=1 ;
     }
-    else if (computer === "Paper" && player === "Scissors") {
-        alert ("You won, lucky bastard.")
+    else if (computerChoice === "Paper" && userChoice === "Scissors") {
+        console.log(`You won, ${userChoice} beats ${computerChoice}.`);  
+        userPoints += 1;      
     }
-    else if (computer === "Rock" && player === "Paper") {
-        alert ("You won, lucky bastard.")        
+    else if (computerChoice === "Rock" && userChoice === "Paper") {
+        console.log(`You won, ${userChoice} beats ${computerChoice}.`);      
+        userPoints += 1;        
     }
-    else if (computer === "Rock" && player === "Scissors") {
-        alert ("You lost, you're a looser.")
+    else if (computerChoice === "Rock" && userChoice === "Scissors") {
+        console.log(`You lost, ${computerChoice} beats ${userChoice}`);
+        computerPoints +=1 ;
     }
-    else if (computer === "Scissors" && player === "Rock") {
-        alert ("You won, lucky bastard.")        
+    else if (computerChoice === "Scissors" && userChoice === "Rock") {
+        console.log(`You won, ${userChoice} beats ${computerChoice}.`); 
+        userPoints += 1;        
     }
-    else if (computer === "Scissors" && player === "Paper") {
-        alert ("You lost, you're a looser.")
+    else if (computerChoice === "Scissors" && userChoice === "Paper") {
+        console.log(`You lost, ${computerChoice} beats ${userChoice}`);
+        computerPoints +=1 ;
     }
 }
 
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3);
-    let computerChoice = '';
     if (randomNumber == 0) {
         computerChoice = "Rock";
-        document.getElementById("computerChoice").innerHTML = computerChoice;
-        return "Rock";
     }
     else if (randomNumber == 1) {
         computerChoice = 'Paper';
-        document.getElementById("computerChoice").innerHTML = computerChoice;
-        return "Paper";
     } 
-    else if (randomNumber == 0) {
+    else if (randomNumber == 2) {
         computerChoice = 'Scissors';
-        document.getElementById("computerChoice").innerHTML = computerChoice;
-        return "Scissors";
     }
 }
 
+function getUserChoice() {
+    randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber == 0) {
+        userChoice = "Rock";
+    }
+    else if (randomNumber == 1) {
+        userChoice = 'Paper';
+    } 
+    else if (randomNumber == 2) {
+        userChoice = 'Scissors';
+    }
+}
+
+game();
