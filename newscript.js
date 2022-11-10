b8bd6ff3fe6c765dd4dbd8e57b14a8e9943362b9
AdrienSuro <adrien.surowiec@protonmail.com>
2022-11-10 07:08:11 +0100

    Add single round logic with buttons

---
+++ newscript.js
@@ -0,0 +1,10 @@
+const rockBtn = document.querySelector('#rock');
+const firstTitle = document.querySelector('#firstTitle')
+const firstDiv = document.querySelector('div');
+
+rockBtn.setAttribute('style', 'background-color : green;');
+
+firstTitle.style.color = `blue` ;
+firstTitle.style.fontSize = '5px' ;
+
+firstDiv.setAttribute('style', 'background-color : pink; border-width: 10px; border-color: black; border-style: solid; padding: 5px;');
--- script.js
+++ script.js
@@ -3,53 +3,80 @@ let userPoints = 0;
 let computerPoints = 0;
 let userChoice = '';
 let computerChoice = '';
+let computerInput = '';
+let userInput = '';
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const gameDiv = document.querySelector('.game');
+
+rockBtn.addEventListener('click', () => {
+    userChoice = "Rock";
+    singleRound();
+});
+
+paperBtn.addEventListener('click', () => {
+    userChoice = "Paper";
+    singleRound();
+});
+
+scissorsBtn.addEventListener('click', () => {
+    userChoice = "Scissors";
+    singleRound();
+});
+
+
 
 //declare the functions :
-function game() {
-    for (let i = 0; i < 5; i++) {
-        singleRound();
-    }
-    if (userPoints > computerPoints) {
-        console.log("You're the best, you won the game!");
-    }
-    else if (userPoints < computerPoints) {
-        console.log("You lost... Try again!");
-    }
-    else {
-        console.log("Oh oh... Seems like it's a final draw. Equality for all! The communist inside you shall be happy.");
-    }
-}
 
 function singleRound() {
     getComputerChoice();
-    getUserChoice();
+    if (userPoints === 5 && computerPoints === 5) {
+        //...FINISH THIS after single round logic is done
+    }
     if (userChoice === computerChoice) {
-        console.log("It's a draw!");
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `It's a draw!`;
+        gameDiv.appendChild(roundResult);
         userPoints += 1;
         computerPoints += 1;
     }
     else if (computerChoice === "Paper" && userChoice === "Rock") {
-        console.log(`You lost, ${computerChoice} beats ${userChoice}`);
-        computerPoints +=1 ;
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `You lost, ${userChoice} beats ${computerChoice}.` ; 
+        gameDiv.appendChild(roundResult);
+        computerPoints += 1;
     }
+    
     else if (computerChoice === "Paper" && userChoice === "Scissors") {
-        console.log(`You won, ${userChoice} beats ${computerChoice}.`);  
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `You won, ${userChoice} beats ${computerChoice}.` ; 
+        gameDiv.appendChild(roundResult);
         userPoints += 1;      
     }
     else if (computerChoice === "Rock" && userChoice === "Paper") {
-        console.log(`You won, ${userChoice} beats ${computerChoice}.`);      
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `You won, ${userChoice} beats ${computerChoice}.` ; 
+        gameDiv.appendChild(roundResult);     
         userPoints += 1;        
     }
     else if (computerChoice === "Rock" && userChoice === "Scissors") {
-        console.log(`You lost, ${computerChoice} beats ${userChoice}`);
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `You lost, ${userChoice} beats ${computerChoice}.` ; 
+        gameDiv.appendChild(roundResult);
         computerPoints +=1 ;
     }
     else if (computerChoice === "Scissors" && userChoice === "Rock") {
-        console.log(`You won, ${userChoice} beats ${computerChoice}.`); 
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `You won, ${userChoice} beats ${computerChoice}.` ; 
+        gameDiv.appendChild(roundResult);
         userPoints += 1;        
     }
     else if (computerChoice === "Scissors" && userChoice === "Paper") {
-        console.log(`You lost, ${computerChoice} beats ${userChoice}`);
+        const roundResult = document.createElement('p'); 
+        roundResult.textContent = `You lost, ${userChoice} beats ${computerChoice}.` ; 
+        gameDiv.appendChild(roundResult);
         computerPoints +=1 ;
     }
 }
@@ -67,12 +94,7 @@ function getComputerChoice() {
     }
 }
 
-function getUserChoice() {
-    userChoice = prompt("What is your choice for this round ?");
-}
+
+
 
 //run the function : 
-game();
-
-//communicate with the html :
-document.getElementById("computerChoice").innerHTML = computerChoice;
